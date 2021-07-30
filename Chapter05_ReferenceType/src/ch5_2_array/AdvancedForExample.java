eae18721e37defd581ca755b73e8ff18e41660fe
moonhy7 <moonhy7@naver.com>
2021-07-30 22:16:52 +0900

    21.7.30

--- Chapter05_ReferenceType/src/ch5_2_array/AdvancedForExample.java
+++ Chapter05_ReferenceType/src/ch5_2_array/AdvancedForExample.java
@@ -4,5 +4,15 @@ public class AdvancedForExample {
 	public static void main(String[] args) {
 		// for문의 독특한 형태가 나옴
 		// 단점: 읽기만 가능 쓰질 못함
+		int[] scores = {97, 71, 84, 93, 87};
+		
+		int sum = 0;
+		for(int score : scores) {
+			sum = sum + score;
+		}
+		System.out.println("점수 총합 = " + sum);
+		
+		double avg = (double) sum / scores.length;
+		System.out.println("점수 평균 = " + avg);
 	}
 }
--- Self_study/src/reviewProblem/ch05_2.java
+++ Self_study/src/reviewProblem/ch05_2.java
@@ -110,7 +110,6 @@ public class ch05_2 {
 		}
 		System.out.println("프로그램 종료");
 		
-
 	}
 	
 }
