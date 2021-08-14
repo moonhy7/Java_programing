451a4bb16339a886d3948817cd397893f4b8c896
moonhy7 <moonhy7@naver.com>
2021-08-15 00:14:41 +0900

    21.8.15.Sun

--- Chapter07_Inheritance/src/ch07_3_abstract_class/AnimalExample.java
+++ Chapter07_Inheritance/src/ch07_3_abstract_class/AnimalExample.java
@@ -2,6 +2,28 @@ package ch07_3_abstract_class;
 
 public class AnimalExample {
 	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
 		
+		dog.sound();
+		cat.sound();
+		System.out.println("----------");
+		
+		// 변수의 자동 타입 변환
+		Animal animal = null;
+		animal = new Dog();
+		animal.sound();
+		
+		animal = new Cat();
+		animal.sound();
+		System.out.println("----------");
+		
+		// 메소드의 다형성
+		animalSound(new Dog());
+		animalSound(new Dog());		
+	}
+	public static void animalSound(Animal animal) {
+		animal.sound();
 	}
 }
+ 
