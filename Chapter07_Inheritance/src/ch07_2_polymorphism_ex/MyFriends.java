5a22bc00a53e48d34d9d15b15f35100d09bff7c9
moonhy7 <moonhy7@naver.com>
2021-08-15 17:26:40 +0900

    21.8.15.Sun

--- Chapter07_Inheritance/src/ch07_2_polymorphism_ex/MyFriends.java
+++ Chapter07_Inheritance/src/ch07_2_polymorphism_ex/MyFriends.java
@@ -1,61 +1,87 @@
 package ch07_2_polymorphism_ex;
 
-// 대학친구
-class UnivFriend {
-	//필드
-	private String name;
-	private String major;
-	private String phone;
+// 부모 클래스
+class Friend {
+	// 필드
+	public String name;
+	public String phone;
+
+	// 생성자
+	public Friend(String na, String ph) {
+		name = na;
+		phone =ph;
+	}
+
+	// 메소드
+	public void showInfo() {
+		System.out.println("이름 : " + name);
+		System.out.println("전화번호 : " + phone);
+	}
+
+}
+
+// 자식 클래스 - 대학 친구
+class UnivFriend extends Friend {
+	// 필드
+	public String major;
 	
-	//생성자
+	// 생성자
 	public UnivFriend(String na, String ma, String ph) {
-		name = na;
-		major = ma;
-		phone = ph;
+		super(na, ph);
+		major =ma;
 	}
-	//메소드
+	
+	// 메소드
+	@Override
 	public void showInfo() {
-		System.out.println("이름: " + name);
-		System.out.println("전공: " + major);
-		System.out.println("전화: " + phone);
+		super.showInfo();
+		System.out.println("전공 : major");
 	}
 }
 
-// 직장동료
-class CompFriend {
-	//필드
-	private String name;
-	private String department;
-	private String phone;
+// 자식 클래스 - 직장 동료
+class CompFriend extends Friend{
+	// 필드
+	public String department;
 	
-	//생성자
+	// 생성자
 	public CompFriend(String na, String de, String ph) {
-		name = na;
+		super(na, ph);
 		department = de;
-		phone = ph;
 	}
 	
-	//메소드
+	// 메소드
+	@Override
 	public void showInfo() {
-		System.out.println("이름: " + name);
-		System.out.println("부서: " + department);
-		System.out.println("전화: " + phone);
+		super.showInfo();
+		System.out.println("부서 : " + department);
 	}
 }
 
+// 실행 클래스
 public class MyFriends {
 	public static void main(String[] args) {
+		Friend[] friends = new Friend[6];
+		int count=0;
 		
-		//대학 친구의 관리를 위한 배열과 변수
-		UnivFriend[] ufrns = new UnivFriend[5];
-		int ucnt = 0;
+		// 대학친구 추가
+		friends[count++] = new UnivFriend("kim", "computer", "010-1");
+		friends[count++] = new UnivFriend("lee", "electronics", "010-2");
+		friends[count++] = new UnivFriend("seo", "Math", "010-3");
 		
-		//직장 동료의 관리를 위한 배열과 변수
-		CompFriend[] cfns = new CompFriend[5];
-		int ccnt = 0;
+		// 직장동료 추가
+		friends[count++] = new CompFriend("kim", "R&D", "010-1");
+		friends[count++] = new CompFriend("lee", "R&D", "010-2");
+		friends[count++] = new CompFriend("seo", "R&D", "010-3");
 		
-		//대학친구 추가
-//		ufrns[unct++] = new UnivFriend("LEE, ma, ph");
-		
+		for(int i=0; i<friends.length; i++) {
+			friends[i].showInfo();
+			if(friends[i].name == "kim") {
+				System.out.println("kim 을" + i+1 + "번째에서 찾았습니다.");
+			} System.out.println();
+		}
 	}
 }
+
+
+
