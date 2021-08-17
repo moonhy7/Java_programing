4e5e8bc0d8c8e483f98f40990b7209d2a88ca6e2
moonhy7 <moonhy7@naver.com>
2021-08-18 01:34:59 +0900

    21.8.18.Wed

--- Chapter08_Interface/src/ch08_2_polymorphism/DriverExample.java
+++ Chapter08_Interface/src/ch08_2_polymorphism/DriverExample.java
@@ -7,9 +7,11 @@ interface Vehicle {
 
 // 운전자 클래스 - 매개변수로 인터페이스 사용
 class Driver {
-	public void drive(Vehicle vehicle) {
-		if(vehicle instanceof Bus) {
-			((Bus)vehicle).checkFare();
+	public void drive(Vehicle vehicle) { // main 메소드에서 vehicle 자리에 Bus 객체와 Taxi 객체를 보내줄 예정
+		if(vehicle instanceof Bus) { 
+			Bus bus = (Bus) vehicle; // 매개 값으로 Bus 객체가 들어온다면 강제 타입 변환 가능
+			bus.checkFare(); // Bus타입으로 강제형변환을 한 이유로서 Bus 클래스의 메소드를 쓰기위함이다. 
+			// ((Bus)vehicle).checkFare(); // 이렇게 한 줄로 쓸 수도 있음
 			vehicle.run();
 		} else {
 			vehicle.run(); // 인터페이스의 메소드 호출
@@ -45,8 +47,10 @@ public class DriverExample {
 		Bus bus = new Bus(); // 구현 객체 생성
 		Taxi taxi = new Taxi();
 		
-		driver.drive(bus); // 버스가 달립니다.
+		driver.drive(bus); // 승차요금을 체크합니다. 버스가 달립니다.
+		// drive() 메소드의 매개 값으로 bus를 대입했으므로 Bus 타입으로 형변환 가능
 		driver.drive(taxi); // 택시가 달립니다.
+		// taxi는 Bus 타입으로 형변환 안되서 checkFare() 출력안됨
 		
 		Vehicle vehicle = new Bus(); // 자동 형변환
 	}
